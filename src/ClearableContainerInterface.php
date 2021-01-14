71a49070fb9056b1073429d53b5f6db778764304
Anton Ukhanev <xedin.unknown@gmail.com>
2021-01-14 18:22:03 +0100

    Suppressed invalid throw error

--- src/ClearableContainerInterface.php
+++ src/ClearableContainerInterface.php
@@ -12,6 +12,7 @@ interface ClearableContainerInterface extends PsrContainerInterface
     /**
      * Removes all members from this container.
      *
+     * @psalm-suppress InvalidThrow In PSR-11, this interface does not extend `Throwable`.
      * @throws ContainerExceptionInterface If problem removing.
      */
     public function clear(): void;
