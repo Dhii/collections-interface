476dd0eeb20b2963517a6dfc939e52e634f3cc6b
Anton Ukhanev <xedin.unknown@gmail.com>
2026-03-24 01:07:29 +0100

    Declare typehint

--- src/WritableContainerFactoryInterface.php
+++ src/WritableContainerFactoryInterface.php
@@ -14,7 +14,14 @@ interface WritableContainerFactoryInterface extends ContainerFactoryInterface
     /**
      * @inheritDoc
      *
-     * @return WritableContainerInterface The new container.
+     * @template K of string
+     * @template V of mixed
+     *
+     * @param array<K, V> $data The data for the container.
+     *
+     * @return WritableContainerInterface<K, V> The new container.
+     *
+     * @psalm-suppress MoreSpecificImplementedParamType PSR-11 does not declare generics, but should
      */
     #[\Override]
     public function createContainerFromArray(array $data): ContainerInterface;
